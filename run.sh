39ac5bf5e95d465862f75472dfd2e91df0d3b9f8
Jeryn Mathew <jeryn.mathew@taxiforsure.com>
2014-11-02 16:27:52 +0000

    Bugfix: Error in run script.

--- run.sh
+++ run.sh
@@ -2,13 +2,10 @@
 
 set -e
 
-USER=${ELASTICSEARCH_USER:-admin}
-echo "=> Starting Elasticsearch with basic auth ..."
-echo ${ELASTICSEARCH_PASS} | htpasswd -i -c /htpasswd ${USER}
 echo "========================================================================"
 echo "You can now connect to this Elasticsearch Server using:"
 echo ""
-echo "    curl ${USER}:${ELASTICSEARCH_PASS}@localhost:9200"
+echo "    curl localhost:9200"
 echo ""
 echo "========================================================================"
 exec service supervisor restart
